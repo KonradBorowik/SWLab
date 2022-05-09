95cb9f004a764b045f482f4c5d27ee104f97f8d7
Konrad <konrad1borowik@gmail.com>
2022-05-09 20:03:23 +0200

    legos filled

--- projektas.py
+++ projektas.py
@@ -14,29 +14,39 @@ import numpy as np
 #     cv2.waitKey()
 
 
-def concept_1():
-    img = cv2.imread(r"pictures\project\img_018.jpg")
+def brighten_dark_spots():
+    img = cv2.imread(r"pictures\project\img_012.jpg")
     img = cv2.resize(img, (0, 0), fx=0.2, fy=0.2)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    img_gray_blurred = cv2.GaussianBlur(img_gray, (3, 3), 0)
-    img_canny = cv2.Canny(img_gray_blurred, 15, 30)
 
-    kernel = np.ones((5, 5), np.uint8)
-    dilate = cv2.dilate(img_canny, kernel)
 
-    close = cv2.morphologyEx(dilate, cv2.MORPH_CROSS, (5, 5))
+    cv2.imshow("original", img)
+    cv2.waitKey()
 
-    # output = cv2.connectedComponentsWithStats(close, cv2.CV_32S)
+
+def concept_1():
+    img = cv2.imread(r"pictures\project\img_016.jpg")
+    img = cv2.resize(img, (0, 0), fx=0.2, fy=0.2)
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img_gray_blurred = cv2.GaussianBlur(img, (5, 5), 0)
+    img_canny = cv2.Canny(img_gray_blurred, 15, 20, cv2.THRESH_OTSU)
+
+    kernel_dilate = np.ones((3, 3), np.uint8)
+    dilate = cv2.dilate(img_canny, kernel_dilate)
+
+    # close = cv2.morphologyEx(dilate, cv2.MORPH_OPEN, (5, 5))
+
+    # output = cv2.connectedComponentsWithStats(erode, cv2.CV_32S)
     # (numLabels, labels, stats, centroids) = output
 
-    contours, hierarchy = cv2.findContours(close, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(img, contours, -1, (255, 0, 0), thickness=cv2.FILLED)
+    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img, contours, -1, (255, 0, 0), -1)
 
     cv2.imshow("original image", img)
     cv2.imshow("blurred", img_gray_blurred)
     cv2.imshow("canny", img_canny)
-    cv2.imshow("close", close)
+    # cv2.imshow("close", close)
     cv2.imshow("dilate", dilate)
     cv2.waitKey()
 
@@ -44,6 +54,7 @@ def concept_1():
 if __name__ == "__main__":
     # isolate_white_blocks()
     concept_1()
+    # brighten_dark_spots()
 
 
 # porównywanie kształtów przy pomocy momentów B)
